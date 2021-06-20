08d77224b89f61d64b7ec6c14f034ad6d96f39c1
rapthi <thierry@rapillard.ch>
2021-06-20 15:14:59 +0200

    Tests

--- src/test/groovy/ch/rapillard/VirtualMachinesControllerTest.groovy
+++ src/test/groovy/ch/rapillard/VirtualMachinesControllerTest.groovy
@@ -1,11 +1,15 @@
 package ch.rapillard
+
+import ch.rapillard.domain.VirtualMachine
+import ch.rapillard.services.VirshService
+import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Test
 import io.micronaut.http.client.annotation.*
 import javax.inject.Inject
-import static org.junit.jupiter.api.Assertions.*
+import io.micronaut.http.HttpRequest
 
 @MicronautTest
 public class VirtualMachinesControllerTest {
@@ -14,8 +18,22 @@ public class VirtualMachinesControllerTest {
     @Client("/")
     RxHttpClient client
 
+    @Inject
+    VirshService vmService
+
     @Test
     void testIndex() throws Exception {
         assert client.toBlocking().exchange("/virtualMachines").status() == HttpStatus.OK
+    }
+
+    @Test
+    void testContentOfVms() throws Exception {
+        def machines = vmService.getAll()
+        HttpRequest request = HttpRequest.GET("/virtualMachines")
+        List<VirtualMachine> response = client
+                .toBlocking()
+                .retrieve(request, Argument.of(List.class, VirtualMachine.class))
+
+        assert machines.size() == response.size()
     }
 }
